4af7f2db8a159dd646659675bc604b7b5785287d
Johngtka <johngtka@gmail.com>
2024-09-07 01:39:02 +0200

    feat: added translate to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,42 @@
 import { NgModule } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
+import { TranslateModule } from '@ngx-translate/core';
+import { TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+
+export function HttpLoaderFactory(httpClient: HttpClient) {
+    return new TranslateHttpLoader(httpClient, './assets/i18n/');
+}
+
+const navigatorLang = navigator.language.split('-')[0];
+const supportedLang = ['pl', 'en'];
+const lang = supportedLang.includes(navigatorLang) ? navigatorLang : 'pl';
+const materialModules = [MatProgressSpinnerModule];
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideAnimationsAsync()
-  ],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, HomeComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        TranslateModule.forRoot({
+            defaultLanguage: lang,
+            loader: {
+                provide: TranslateLoader,
+                useFactory: HttpLoaderFactory,
+                deps: [HttpClient],
+            },
+        }),
+        ...materialModules,
+    ],
+    providers: [provideAnimationsAsync()],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
