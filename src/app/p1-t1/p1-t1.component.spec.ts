5a3e58bb68c344e188a0612f33f0e5bcd15aae78
Johngtka <johngtka@gmail.com>
2024-09-24 19:25:48 +0200

    style: prettier code

--- src/app/p1-t1/p1-t1.component.spec.ts
+++ src/app/p1-t1/p1-t1.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { P1T1Component } from './p1-t1.component';
 
 describe('P1T1Component', () => {
-  let component: P1T1Component;
-  let fixture: ComponentFixture<P1T1Component>;
+    let component: P1T1Component;
+    let fixture: ComponentFixture<P1T1Component>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [P1T1Component]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [P1T1Component],
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(P1T1Component);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(P1T1Component);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
