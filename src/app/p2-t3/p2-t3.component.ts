42d99dce021de52dbdbf2573e10aab6e396342e6
Johngtka <johngtka@gmail.com>
2024-10-17 22:34:38 +0200

    feat: added new P2 components to app

---
+++ src/app/p2-t3/p2-t3.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-p2-t3',
+  templateUrl: './p2-t3.component.html',
+  styleUrl: './p2-t3.component.css'
+})
+export class P2T3Component {
+
+}
